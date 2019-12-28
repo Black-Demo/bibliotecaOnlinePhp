411a7eb4db80649d210edb5db4b98e84db508139
BlackDemo <stiven.miranda.asprilla@gmail.com>
2019-12-28 23:33:20 +0100

    Mover el script al footer
    Mover el script al footer para permiterque sea usado en todas las paginas

--- footer.php
+++ footer.php
@@ -1,2 +1,17 @@
     <footer>@BlackDemo 2019 IES Joan Ramis i Ramis</footer>
+    <!--
+        Script to put a class into the iinputs 
+        to aplicate the css effect form 
+    -->
+    <script type="text/javascript">
+        $(".txtb input").on("focus",function() {
+            $(this).addClass("focus");
+        });
+
+        $(".txtb input").on("blur",function() {
+            if($(this).val() == "")
+                $(this).removeClass("focus");
+        })
+    </script>
+    
 </body>
