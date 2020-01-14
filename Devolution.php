e72c5efca2b6e71f27569bd4e0df4c62e4616906
BlackDemo <stiven.miranda.asprilla@gmail.com>
2020-01-14 20:57:58 +0100

    añadir el botón para comprar libros

--- Devolution.php
+++ Devolution.php
@@ -19,20 +19,30 @@
                 WHERE dni = '$varDni' and date_devolution is null";
 
             $user = mysqli_fetch_all(mysqli_query($conn,$SelectUser), MYSQLI_ASSOC);
-
-            foreach($user as $userBook){
-                echo 'the user '.$userBook['name'].' whit dni: '.$userBook['dni']
-                    .'<br>Need to return '.$userBook['title'].' write in '.$userBook['languages']
-                    .'<br>Reserved: '.$userBook['date_reserve'].' and time limit: '.$userBook['date_end'];
-                echo "
-                    <form name='libro' method='POST' action='Reservations/returnReserve.php'>
-                        <input type='hidden' id='date_end' name='date_end' value='$userBook[date_end]'>
-                        <input type='hidden' id='user_id' name='user_id' value='$userBook[user_id]'>
-                        <input type='hidden' id='Copybook_id' name='Copybook_id' value='$userBook[Copybook_id]'>
-                        <input type='submit' name='return' value='Return' class='btn'>
-                    </form>";
-                echo '<br>';
-            }            
+            if(count($user)>0){
+                foreach($user as $userBook){
+                    echo 'the user '.$userBook['name'].' whit dni: '.$userBook['dni']
+                        .'<br>Need to return '.$userBook['title'].' write in '.$userBook['languages']
+                        .'<br>Reserved: '.$userBook['date_reserve'].' and time limit: '.$userBook['date_end'];
+                    echo "
+                        <form name='libro' method='POST' action='Reservations/returnReserve.php'>
+                            <input type='hidden' id='date_end' name='date_end' value='$userBook[date_end]'>
+                            <input type='hidden' id='user_id' name='user_id' value='$userBook[user_id]'>
+                            <input type='hidden' id='Copybook_id' name='Copybook_id' value='$userBook[Copybook_id]'>
+                            <input type='submit' name='return' value='Return' class='btn'>
+                        </form>";
+                    echo '<br>';
+                } 
+            }else{
+                echo "<p class='alert'>None with this DNI: $varDni need to return a book</p>";
+                ?>
+                    <form action="Devolution.php" method='POST' class="searchDNI-box">
+                        <input type="text" class="searchDNI-txt"  placeholder="Dni user" name="dni">
+                        <input type="submit" value="search" name="searchDNI" class="searchDNI-btn">
+                    </form>
+                <?php
+            }
+                       
         }
     ?>
 </div>
