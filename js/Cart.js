5950e3049127e4adf27d317ba1f399916f86b81c
BlackDemo <stiven.miranda.asprilla@gmail.com>
2020-02-05 20:03:32 +0100

    Revert "estructura de cada elemento"
    This reverts commit adb1470057b556ccfff7941ab6684a79b99e4edb.

--- js/Cart.js
+++ js/Cart.js
@@ -1,7 +1,9 @@
 var books = new Array();
 
-for (let i = 0; i < localStorage.length; i++) {
-  let key = localStorage.key(i);
-  let product = localStorage.getItem(key);
-  books.push(JSON.parse(product));
+function getProdcuts() {
+  for (let i = 0; i < localStorage.length; i++) {
+    let key = localStorage.key(i);
+    let product = localStorage.getItem(key);
+    books.push(JSON.parse(product));
+  }
 }
