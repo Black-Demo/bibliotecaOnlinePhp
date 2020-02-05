0ae499cedee984edc9f1630f2cb0d6b348cc39ac
BlackDemo <stiven.miranda.asprilla@gmail.com>
2020-02-05 20:04:14 +0100

    estructura de cada elemento

--- shoppingCard.php
+++ shoppingCard.php
@@ -1,8 +1,26 @@
 <?php 
     include 'header.php';
 ?>
+<div class="container">
+    <div class="img">
+        <img src="src/userImg/no-img.jpg" alt="" width=50px>
+    </div>
+    <div class="quanttiy">
+        <i>2</i>
+        <div class="up-down">
+            <i class="fas fa-sort-up"></i>
+            <i class="fas fa-sort-down"></i>
+        </div>
+    </div>
+    <div class="price">
+        <i>10,20</i>
+        <i>€</i>
+    </div>
 
-
+    <div class="total">
+        <i>20,34</i> <i>€</i>
+    </div>
+</div>
 <?php
     include 'footer.php';
 ?>
