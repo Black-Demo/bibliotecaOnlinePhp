adb1470057b556ccfff7941ab6684a79b99e4edb
BlackDemo <stiven.miranda.asprilla@gmail.com>
2020-02-05 20:03:32 +0100

    estructura de cada elemento

--- js/Cart.js
+++ js/Cart.js
@@ -1,9 +1,7 @@
 var books = new Array();
 
-function getProdcuts() {
-  for (let i = 0; i < localStorage.length; i++) {
-    let key = localStorage.key(i);
-    let product = localStorage.getItem(key);
-    books.push(JSON.parse(product));
-  }
+for (let i = 0; i < localStorage.length; i++) {
+  let key = localStorage.key(i);
+  let product = localStorage.getItem(key);
+  books.push(JSON.parse(product));
 }
