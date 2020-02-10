b4d4496d5637ffb08b1bcf096a82af5a9c3a0fc3
BlackDemo <stiven.miranda.asprilla@gmail.com>
2020-02-10 17:17:12 +0100

    Cambios en el carrito de la compra

--- js/Cart.js
+++ js/Cart.js
@@ -12,43 +12,104 @@ function getProdcuts() {
 }
 function chargeCart() {
   getProdcuts();
-  // let contTotal = document.createElement("div");
+  let contTotal = document.createElement("div");
+  let currency = document.createElement("i");
+  $(currency).text("€");
   for (let i = 0; i < books.length; i++) {
+    if (books[i].img == "") books[i].img = "no-img.jpg";
     let cont = document.createElement("div");
     let contImg = document.createElement("div");
-    let contInfo = document
-      .createElement("div")
-      .setAttribute("class", "infoItem");
-    let contQuatity = document
-      .createElement("div")
-      .setAttribute("class", "quantity");
-    let contPrice = document
-      .createElement("div")
-      .setAttribute("class", "price");
-    let contDelete = document
-      .createElement("div")
-      .setAttribute("class", "deleteIcon");
+    let contInfo = document.createElement("div");
+    let contQuatity = document.createElement("div");
+    let contPrice = document.createElement("div");
+    let contDelete = document.createElement("div");
 
     let img = document.createElement("img");
+    $(img).attr({ src: "src/userImg/" + books[i].img, width: "50px" });
+
     let title = document.createElement("p");
+    $(title)
+      .addClass("title")
+      .text(books[i].title);
+
     let info = document.createElement("i");
+    $(info)
+      .addClass("information")
+      .text("write by: " + books[i].author);
+
     let arrows = document.createElement("div");
     let up = document.createElement("i");
+    $(up)
+      .addClass("fas fa-sort-up")
+      .on("click", function() {
+        books[i].quantity++;
+        $(quantity).text(books[i].quantity);
+        $(price).text(
+          (parseInt(books[i].quantity) * parseFloat(books[i].price)).toFixed(2)
+        );
+      });
     let down = document.createElement("i");
+    $(down)
+      .addClass("fas fa-sort-down")
+      .on("click", function() {
+        if (books[i].quantity > 1) books[i].quantity--;
+        else deleteElement();
+        $(quantity).text(books[i].quantity);
+        $(price).text(
+          (parseInt(books[i].quantity) * parseFloat(books[i].price)).toFixed(2)
+        );
+      });
+    $(arrows)
+      .addClass("up-down")
+      .append([up, down]);
     let quantity = document.createElement("i");
+    $(quantity).text(books[i].quantity);
+
     let price = document.createElement("i");
-    let currency = document.createElement("i");
+
+    $(price).text(
+      (parseInt(books[i].quantity) * parseFloat(books[i].price)).toFixed(2)
+    );
+
     let deleteIcon = document.createElement("i");
+    $(deleteIcon)
+      .addClass("fas fa-trash-alt")
+      .on("click", deleteElement);
 
     let otherItem = document.createElement("div");
+    $(otherItem).addClass("otherItem");
 
-    $(cont)
+    $(contImg)
+      .addClass("img")
+      .append(img);
+    $(contInfo)
+      .addClass("infoItem")
+      .append([title, info]);
+    $(contQuatity)
+      .addClass("quanttiy")
+      .append([arrows, quantity]);
+    $(contPrice)
+      .addClass("price")
+      .append([price, currency]);
+    $(contDelete)
+      .addClass("deleteIcon")
+      .append(deleteIcon);
+
+    $(cont)v
       .addClass("itemCart")
-      .append([$(contImg).addClass("test"), $(img).addClass("test23")]);
+      .attr("id", i)
+      .append([contImg, contInfo, contQuatity, contPrice, contDelete]);
     $(document.body)
       .find(".shoppingCart")
-      .append(cont);
+      .append([cont, otherItem]);
   }
+  
+  $(document.body).find(".shoppingCart").append(contTotal);
+}
+
+
+function deleteElement(){
+  alert(this);
 }
 
 chargeCart();
